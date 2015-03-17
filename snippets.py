13696b7e8ab9baaaf32a0dabcd8923e0456bb64d
Sam Sena <samsena@gmail.com>
2015-03-17 03:41:33 +0000

    Added cursor as context manager

--- snippets.py
+++ snippets.py
@@ -7,30 +7,43 @@ logging.debug("Connecting to PostgreSQL")
 connection = psycopg2.connect("dbname='snippets' user='action' host='localhost'")
 logging.debug("Database connection established.")
 
+
+
+# def put(name, snippet):
+#   """Store a snippet with an associated name."""
+#   logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
+#   cursor = connection.cursor()
+#   command = "insert into snippets values (%s, %s)"
+#   try:
+#     command = "insert into snippets values (%s, %s)"
+#     cursor.execute(command, (name, snippet))
+#   except psycopg2.IntegrityError as e:
+#     connection.rollback()
+#     command = "update snippets set message=%s where keyword=%s"
+#     cursor.execute(command, (name, snippet))
+#   connection.commit()
+#   logging.debug("Snippet stored successfully.")
+#   return name, snippet
+
 def put(name, snippet):
   """Store a snippet with an associated name."""
   logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
-  cursor = connection.cursor()
-  command = "insert into snippets values (%s, %s)"
-  try:
-    command = "insert into snippets values (%s, %s)"
-    cursor.execute(command, (name, snippet))
-  except psycopg2.IntegrityError as e:
-    connection.rollback()
-    command = "update snippets set message=%s where keyword=%s"
-    cursor.execute(command, (name, snippet))
-  connection.commit()
+  with connection, connection.cursor() as cursor:
+    try:
+      cursor.execute("insert into snippets values (%s, %s)", (name, snippet))
+    except psycopg2.IntegrityError as e:
+      connection.rollback()
+      cursor.execute("update snippets set message=%s where keyword=%s", (name, snippet))
+      connection.commit()
   logging.debug("Snippet stored successfully.")
   return name, snippet
 
-  
 def get(name):
   """Retrieve the snippet with a given name."""
   logging.info("Retrieving snippet {!r}".format(name))
-  cursor = connection.cursor()
-  command = "select message from snippets where keyword=%s"
-  cursor.execute(command, (name,))
-  row = cursor.fetchone()
+  with connection, connection.cursor() as cursor:
+    cursor.execute("select message from snippets where keyword=%s", (name,))
+    row = cursor.fetchone()
   logging.debug("Snippet retrieved successfully.")
   if not row:
     # No snippet was found with that name.
